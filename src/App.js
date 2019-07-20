155d6f224a206db389700e8bca3005f92eaeb090
KnifeParty12 <flashfunk01@gmail.com>
2019-07-21 02:59:15 +0530

    Initialized Project and dependencies
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {useEffect} from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
+import './App.css';
+
+const App = () => {
+  useEffect(()=>{
+    // Init Materialize JS
+    M.AutoInit();
+  });
+  return (
+    <div className="App">
+      My App
+    </div>
+  );
+};
+
+export default App;
