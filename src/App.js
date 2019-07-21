0251984dd80a78af09bc32010ce2a15278b9baa4
KnifeParty12 <flashfunk01@gmail.com>
2019-07-21 15:52:05 +0530

    Added form for adding techs and logs
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {useEffect,Fragment} from 'react';
 import Logs from './components/logs/Logs';
 import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import SearchBar from './components/layout/SearchBar';
@@ -14,6 +15,7 @@ const App = () => {
     <SearchBar/>
     <div className="container">
       <AddBtn/>
+      <AddLogModal/>
         <Logs/>
     </div>
   </Fragment>;
