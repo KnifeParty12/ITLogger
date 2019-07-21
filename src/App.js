51097c59a47b0cc045f01a03ef0c0ff10f7fb5d7
KnifeParty12 <flashfunk01@gmail.com>
2019-07-21 14:33:03 +0530

    Backend connected successfully
    Signed-off-by: KnifeParty12 <flashfunk01@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React, {useEffect} from 'react';
+import React, {useEffect,Fragment} from 'react';
+import Logs from './components/logs/Logs';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
-import './App.css';
+import SearchBar from './components/layout/SearchBar';
 
 const App = () => {
   useEffect(()=>{
     // Init Materialize JS
     M.AutoInit();
   });
-  return (
-    <div className="App">
-      My App
+  return <Fragment>
+    <SearchBar/>
+    <div className="container">
+        <Logs/>
     </div>
-  );
+  </Fragment>;
 };
 
 export default App;
